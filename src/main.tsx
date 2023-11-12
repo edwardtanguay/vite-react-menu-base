acaa343897ebe1ddf6320abea2c6db0b9197ee29
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-12 21:07:56 +0100

    added 404 page

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,33 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
 import "./index.scss";
 import { PageWelcome } from "./pages/PageWelcome.tsx";
 import { PageInfo } from "./pages/PageInfo.tsx";
 import { PageAbout } from "./pages/PageAbout.tsx";
+import { Page404 } from "./pages/Page404.tsx";
 
 const router = createBrowserRouter([
 	{
 		path: "/",
+		errorElement: <Page404 />,
 		element: <App />,
 		children: [
 			{
-				path: '/',
+				path: "/welcome",
 				element: <PageWelcome />,
 			},
 			{
-				path: 'info',
+				path: "info",
 				element: <PageInfo />,
 			},
 			{
-				path: 'about',
+				path: "about",
 				element: <PageAbout />,
+			},
+			{
+				path: "/",
+				element: <Navigate to="/welcome" replace />,
 			},
 		],
 	},
