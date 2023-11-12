00959acae30c872fd1c885ab8c5f6798bf17b356
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-12 07:53:14 +0100

    finished

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 
 export const Nav = () => {
 	return <nav>
-		<ul className="flex gap-2 bg-slate-200 p-1 content">
+		<ul className="flex gap-2 bg-slate-500 p-1 content">
 			<li><NavLink to="/">Welcome</NavLink></li>
 			<li><NavLink to="/info">Info</NavLink></li>
 			<li><NavLink to="/about">About</NavLink></li>
