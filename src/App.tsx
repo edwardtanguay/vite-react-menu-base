46ef91a6ff835ac06895e8d51dd2ad2f4ca8289a
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-15 00:20:29 +0200

    responsive

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import { Header } from "./components/Header";
 
 function App() {
 	return (
-		<>
+		<main className="bg-slate-400 p-4 w-full md:w-[60rem] mt-0 md:mt-6">
 			<Header />
 			<main className="py-4">
 				<Outlet />
 			</main>
-		</>
+		</main>
 	);
 }
 
